cd8bab79316bb7c4ffbb8572d907e218fe704a7d
NemesisX1 <junior.medehou00@gmail.com>
2022-09-16 19:49:51 +0100

    (add) fullly - ou presque - functionnal animation of monster

--- game_experiment_00/lib/main.dart
+++ game_experiment_00/lib/main.dart
@@ -1,19 +1,36 @@
-import 'dart:html';
-
+import 'dart:developer';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(GameWidget(game: MyGame()));
 }
 
+enum AnimationStep {
+  idle,
+  left,
+  right,
+  attack,
+}
+
 class MyGame extends FlameGame with SingleGameInstance, KeyboardEvents {
   late Sprite player;
+  Vector2 playerPosition = Vector2(100, 100);
+  Vector2 updatedPosition = Vector2(100, 100);
+  AnimationStep step = AnimationStep.idle;
 
-  late SpriteAnimation a;
+  AnimationStep updatedStep = AnimationStep.idle;
+
+  late SpriteAnimation idle;
+  late SpriteAnimation left;
+  late SpriteAnimation right;
+  late SpriteAnimation attack;
+
+  int speed = 4;
 
   MyGame() {
     // a = SpriteAnimation.spriteList(sprites, stepTime: stepTime)
@@ -26,13 +43,63 @@ class MyGame extends FlameGame with SingleGameInstance, KeyboardEvents {
   }
 
   @override
-  Future<void>? onLoad() async {
-    final playerImage = await images.load('Pink_Monster_Attack1_4.png');
+  KeyEventResult onKeyEvent(
+    RawKeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
+    final isArrowLeftPressed =
+        keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    if (keysPressed.isEmpty) {
+      updatedStep = AnimationStep.idle;
+    }
+    if (keysPressed.contains(LogicalKeyboardKey.keyA)) {
+      updatedStep = AnimationStep.attack;
+    }
+    if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
+      updatedPosition.x += speed;
+      updatedStep = AnimationStep.right;
+    }
+    if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
+      updatedPosition.x -= speed;
+      updatedStep = AnimationStep.left;
+    }
+    return KeyEventResult.ignored;
+  }
 
-    a = SpriteAnimation.fromFrameData(
-      playerImage,
+  @override
+  Future<void>? onLoad() async {
+    final playerImage = await images.load('Pink_Monster_Walk_6.png');
+
+    right = await SpriteAnimation.load(
+      'Pink_Monster_Walk_6.png',
+      SpriteAnimationData.sequenced(
+        amount: 6,
+        textureSize: Vector2(32, 32),
+        stepTime: 0.1,
+      ),
+    );
+
+    left = await SpriteAnimation.load(
+      'Pink_Monster_Walk_Left_6.png',
+      SpriteAnimationData.sequenced(
+        amount: 6,
+        textureSize: Vector2(32, 32),
+        stepTime: 0.1,
+      ),
+    );
+
+    idle = await SpriteAnimation.load(
+      'Pink_Monster_Idle_4.png',
       SpriteAnimationData.sequenced(
         amount: 4,
+        textureSize: Vector2(32, 32),
+        stepTime: 0.1,
+      ),
+    );
+    attack = await SpriteAnimation.load(
+      'Pink_Monster_Attack2_6.png',
+      SpriteAnimationData.sequenced(
+        amount: 6,
         textureSize: Vector2(32, 32),
         stepTime: 0.1,
       ),
@@ -42,15 +109,53 @@ class MyGame extends FlameGame with SingleGameInstance, KeyboardEvents {
   @override
   void render(Canvas canvas) {
     // player.render(canvas);
-    a.getSprite().render(
-          canvas,
-          position: Vector2(100, 100),
-          size: Vector2(100, 100),
-        );
+
+    switch (step) {
+      case AnimationStep.idle:
+        idle.getSprite().render(
+              canvas,
+              position: playerPosition,
+              size: Vector2(100, 100),
+            );
+        break;
+      case AnimationStep.left:
+        left.getSprite().render(
+              canvas,
+              position: playerPosition,
+              size: Vector2(100, 100),
+            );
+        break;
+      case AnimationStep.right:
+        right.getSprite().render(
+              canvas,
+              position: playerPosition,
+              size: Vector2(100, 100),
+            );
+        break;
+      case AnimationStep.attack:
+        attack.getSprite().render(
+              canvas,
+              position: playerPosition,
+              size: Vector2(100, 100),
+            );
+        break;
+      default:
+    }
   }
 
   @override
   void update(double dt) {
-    a.update(dt);
+    idle.update(dt);
+    left.update(dt);
+    right.update(dt);
+    attack.update(dt);
+    playerPosition = updatedPosition;
+    step = updatedStep;
+    // switch (step) {
+    //   case value:
+
+    //     break;
+    //   default:
+    // }
   }
 }
---
+++ game_experiment_00/lib/sources/monster.dart
@@ -0,0 +1,19 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class Monster extends SpriteAnimationComponent with KeyboardHandler {
+  @override
+  Future<void>? onLoad() {
+    // TODO: implement onLoad
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    // TODO: implement update
+    super.update(dt);
+  }
+
+  @override
+  runder(Canvas canvas) {}
+}
